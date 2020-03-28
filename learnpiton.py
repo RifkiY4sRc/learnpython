3d0c0735b3d5a1cafa4606657a5312c3f237b6e2
Rifki <50134810+RifkiY4sRc@users.noreply.github.com>
2020-03-28 07:59:44 +0700

    Add files via upload

---
+++ learnpiton.py
@@ -0,0 +1,126 @@
+                # Beginner Python
+# Penulisan Statement pada python
+'''
+print ('Hello Word') 
+print ("Heloo Word")
+'''
+ # Penulisan String pada python 
+'''
+judul = 'Belajar Python'
+author ="Tutorial Sederhana"
+print (author)
+'''
+
+'''
+alamat = """
+   Nama : Rifki
+   Kelas : TKJ 
+   Sekolah :
+   Alamat :  """
+print (alamat)
+ '''
+'''              
+                                  # Tipe data Text
+# tipe data char (text)
+huruf = "R"
+# tipe data string 
+ judul =" Belajar Python Dasar"
+ # tipe data angaka integer (angka)
+ no = 60
+ no = 50
+ # tipe data float 
+ no = 10,2
+ no = 50,1
+ '''
+ # Operasi Arimatika 
+  # Penjumlahan Simbol
+'''
+a = input ("Masukan Nilai A : ")
+b = input ( "Masukan Nilai B : ")
+c = int (a)+ int (b)
+print ('hasil dari ',a,'+,'b'=',c)
+
+a = input ("Masukan Nilai A : ")
+b = input ( "Masukan Nilai B : ")
+c = int (a) - int (b)
+print ('hasil dari ',a,' - ,'b'=',c)
+'''
+#     Percahangan pada python 
+# Percabagan IF (jika)
+'''
+'nama = 'aska'
+if nama is 'jejak':
+ print ('Selamat Datang',nama)
+ ''' 
+ # Percabangan if /else (tetapi) 
+'''
+nama = 'aska'
+if nama is 'jejak':
+ print ('Selamat Datang',nama)
+else :
+	print ('tolong cek nama anda')
+ '''
+ # Percabangan if (elif/else)
+ 
+'''
+nama = 'Andi'
+if nama is 'jejak':
+ print ('Selamat Datang',nama)
+elif nama is 'Andi':
+ 	print ('selamat datang',nama)
+else:
+ 	print ('tolong cek nama anda')
+ 	
+'''
+# Perulangan pada python
+'''
+ulang = input ('jumlah perulangan: ')
+for i in range (int(ulang)):
+  	i=+1
+print ('Perulangan ke ',i)
+'''
+# List (hurup)
+'''
+nama_hewan = ['ayam','kambing','sapi']
+for i in nama_hewan:
+	print ('Nama hewam',nama_hewan)
+	'''
+	# Perulangan menggunakan while
+'''
+ulang = True
+while ulang :
+	print ('Perulangan dengan while')
+hitung = 0
+while ulang :
+	hitung +=i
+	jawab = input ('Apakah kalian ingin berhenti (y/t)?')
+	print ('Perulangam dengan while')
+if hitung ==10:
+ break
+ '''
+#   Fungsi pada python
+#cara membuat fungsi
+'''
+def salam():
+ 	print ('selamat Datang')
+ 	print ('Kita sedang belajar fungsi')
+salam()
+salam()
+'''
+#fungsi dengan parameter
+
+'''
+def ucapan (ungkapan):
+ 	print ('Selamat',ungkapan)
+ucapan ('pagi')
+'''
+# Pengenalan Class pada python
+'''
+class salam():
+  def ucapan (self):
+      print  ('selamat datang woy')
+  
+  def ungkapan (self):
+  	print ('kita sedang belajar py')
+salam().ucapan()
+ '''
